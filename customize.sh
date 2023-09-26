098c69a588a8ee7fa4776816ba7938b1bb0a9963
Patrick Brisacier (ynh01) <pbrisacier@mncc.fr>
2023-09-26 08:06:08 +0000

    customize EN and FR descriptions

--- customize.sh
+++ customize.sh
@@ -3,8 +3,10 @@
 function show_help {
   pwd
   printf "Used to customize the YunoHost app installation.\n \
-          \t-h|--help: this help menu\n\
-          \t-a|--app-id: change app id in manifest.toml\n"
+          \t-a|--app-id: change app id in manifest.toml\n\
+          \t--desc-en: change English description in manifest.toml\n\
+          \t--desc-fr: change French description in manifest.toml\n\
+          \t-h|--help: this help menu\n"
 }
 
 function log {
@@ -16,6 +18,20 @@ function change_app_id {
   log "Change app ID to [${app_id}]"
 
   sed -ri 's/^(id = "docker_compose"$)/id = \"'"${app_id}"'\"/' ./manifest.toml
+}
+
+function change_desc_en {
+  local desc_en=$1
+  log "Change English description to [${desc_en}]"
+
+  sed -ri 's/^description.en\s*=\s*".*"\s*$/description.en = \"'"${desc_en}"'\"/' ./manifest.toml
+}
+
+function change_desc_fr {
+  local desc_fr=$1
+  log "Change French description to [${desc_fr}]"
+
+  sed -ri 's/^description.fr\s*=\s*".*"\s*$/description.fr = \"'"${desc_fr}"'\"/' ./manifest.toml
 }
 
 # Validate number of arguments
@@ -36,6 +52,14 @@ do
       -a|--app-id)
       shift
       change_app_id $1
+      ;;
+      --desc-en)
+      shift
+      change_desc_en "$1"
+      ;;
+      --desc-fr)
+      shift
+      change_desc_fr "$1"
       ;;
       *)
       show_help
